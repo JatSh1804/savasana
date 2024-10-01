8576f6351119cc78eea03f4acf84b7fd86afdb31
jatin <jatin1804sharma@gmail.com>
2024-10-01 14:48:34 +0530

    Image remote Patterns

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        remotePatterns: ['lh3.googleusercontent.com']
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: ['lh3.googleusercontent.com']
+        },]
     }
 };
 // https://lh3.googleusercontent.com/pw/AP1GczNTV486nBxfzYRbsNETqPVqAFhfH9zBypOZpu775N6bvwA8Bq1dJk2ZcW3STY8vCkp0DoCMHmYdtr_GQOtjUJZfVGIqAOb6Q5UybQLXuXkzD21f-LYVtkkXDGENxH9rlj4fnyYfG_AT-V34HmkT5TOt=w945-h968-s-no-gm?authuser=0
