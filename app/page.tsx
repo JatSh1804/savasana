00458346d19a918e7bbbf3a437a21026624f887f
jatin <jatin1804sharma@gmail.com>
2024-10-01 14:45:37 +0530

    Images working

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Component() {
                                 </div>
                             </div>
                             <Image
-                                src="https://www.curiosityquench.com/_next/image?url=https%3A%2F%2Flh3.googleusercontent.com%2Fpw%2FAP1GczNTV486nBxfzYRbsNETqPVqAFhfH9zBypOZpu775N6bvwA8Bq1dJk2ZcW3STY8vCkp0DoCMHmYdtr_GQOtjUJZfVGIqAOb6Q5UybQLXuXkzD21f-LYVtkkXDGENxH9rlj4fnyYfG_AT-V34HmkT5TOt%3Dw945-h968-s-no-gm%3Fauthuser%3D0&w=640&q=75"
+                                src="https://lh3.googleusercontent.com/pw/AP1GczNTV486nBxfzYRbsNETqPVqAFhfH9zBypOZpu775N6bvwA8Bq1dJk2ZcW3STY8vCkp0DoCMHmYdtr_GQOtjUJZfVGIqAOb6Q5UybQLXuXkzD21f-LYVtkkXDGENxH9rlj4fnyYfG_AT-V34HmkT5TOt=w945-h968-s-no-gm?authuser=0"
                                 width="550"
                                 height="550"
                                 alt="Chatbot"
