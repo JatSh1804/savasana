9999ddabde552f30769b12ef978fd288aba656c0
jatin <jatin1804sharma@gmail.com>
2024-10-01 14:45:15 +0530

    Images working

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    images:{remotePatterns:['curiosityquench.com']}
+    images: {
+        remotePatterns: ['lh3.googleusercontent.com']
+    }
 };
-// curiosityquench.com/_next/image?url=https%3A%2F%2Flh3.googleusercontent.com%2Fpw%2FAP1GczNTV486nBxfzYRbsNETqPVqAFhfH9zBypOZpu775N6bvwA8Bq1dJk2ZcW3STY8vCkp0DoCMHmYdtr_GQOtjUJZfVGIqAOb6Q5UybQLXuXkzD21f-LYVtkkXDGENxH9rlj4fnyYfG_AT-V34HmkT5TOt%3Dw945-h968-s-no-gm%3Fauthuser%3D0&w=640&q=75
-
+// https://lh3.googleusercontent.com/pw/AP1GczNTV486nBxfzYRbsNETqPVqAFhfH9zBypOZpu775N6bvwA8Bq1dJk2ZcW3STY8vCkp0DoCMHmYdtr_GQOtjUJZfVGIqAOb6Q5UybQLXuXkzD21f-LYVtkkXDGENxH9rlj4fnyYfG_AT-V34HmkT5TOt=w945-h968-s-no-gm?authuser=0
 export default nextConfig;
+
+
